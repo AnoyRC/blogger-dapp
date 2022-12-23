45e1941950e20d5db336922917aedf2785bc19d8
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-12-23 14:34:23 +0530

    React and tailwind init

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-5xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
